29e249eb330b04f27e376e0c1a8f3092663c1cf5
Вячеслав Беловолов <samuraiget2008@icloud.com>
2026-09-17 18:33:51 +0500

    Task1

---
+++ Tyuiu.BelovolovVS.Sprint1.Task1.V26.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.BelovolovVS.Sprint1.Task1.V26.Lib;
+namespace Tyuiu.BelovolovVS.Sprint1.Task1.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void VoidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2.0;
+            double y = 3.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(2, res);
+        }
+    }
+}
