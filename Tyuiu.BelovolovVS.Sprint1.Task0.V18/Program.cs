867bb915397f55d0d3ae9ff7da1fe2b5b5fcc610
Вячеслав Беловолов <samuraiget2008@icloud.com>
2026-09-17 17:43:10 +0500

    Task0

--- Tyuiu.BelovolovVS.Sprint1.Task0.V18/Program.cs
+++ Tyuiu.BelovolovVS.Sprint1.Task0.V18/Program.cs
@@ -24,6 +24,7 @@ class Program
         Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
         Console.WriteLine("***************************************************************************");
 
+       
         Console.WriteLine(ds.Calculate());
         Console.ReadLine();
     }
