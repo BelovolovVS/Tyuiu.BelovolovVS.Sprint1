dbe3a3bc60e8988bd6a15ddba5fe436e9ad92c0b
Вячеслав Беловолов <samuraiget2008@icloud.com>
2026-09-24 22:41:20 +0500

    Task2

---
+++ Tyuiu.BelovolovVS.Sprint1.Task2.V29.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.BelovolovVS.Sprint1.Task2.V29.Lib;
+
+namespace Tyuiu.BelovolovVS.Sprint1.Task2.V29.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidConvertSecondsToHours()
+        {
+            DataService ds = new DataService();
+            int value = 7200;
+            int expected = 2;
+
+            int actual = ds.ConvertSecondsToHours(value);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Tyuiu.BelovolovVS.Sprint1.Task2.V29/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Tyuiu.BelovolovVS.Sprint1.Task2.V29.Lib;
+
+namespace Tyuiu.BelovolovVS.Sprint1.Task2.V29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #1 | Выполнил: Беловолов В. С.";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы линейной структуры                                      *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #29                                                             *");
+            Console.WriteLine("* Выполнил: Беловолов Вадим Сергеевич                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Задано количество секунд. Перевести время в полные часы.                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите время в секундах (целое число): ");
+            int seconds = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+            int hours = ds.ConvertSecondsToHours(seconds);
+
+            Console.WriteLine($"Количество полных часов: {hours}");
+
+            Console.WriteLine("***************************************************************************");
+            Console.ReadKey();
+        }
+    }
+}
